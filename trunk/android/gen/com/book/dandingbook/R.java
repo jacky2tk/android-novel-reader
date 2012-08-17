34555ec3e31fdf0a73db9c1e2bf5b85677c257d7
jacky2tk@gmail.com <jacky2tk@gmail.com@56cb43c6-fa1d-8bbd-1b40-62c0ef1cc954>
2012-08-17 07:30:00 +0000

    1. 增加「離線閱讀」頁面 BookRead 2. 設定主畫面「註冊」及「離線閱讀」兩個按鈕, 可切換到各自的畫面 3. 設定「註冊」頁面的「放棄」按鈕, 可切回到主畫面

--- trunk/android/gen/com/book/dandingbook/R.java
+++ trunk/android/gen/com/book/dandingbook/R.java
@@ -23,7 +23,8 @@ public final class R {
         public static final int menu3=0x7f020004;
     }
     public static final class id {
-        public static final int TableLayout1=0x7f08000e;
+        public static final int LinearLayout1=0x7f08000e;
+        public static final int TableLayout1=0x7f080010;
         public static final int TableRow01=0x7f080006;
         public static final int TableRow02=0x7f080003;
         public static final int TableRow03=0x7f080000;
@@ -31,25 +32,29 @@ public final class R {
         public static final int TextView02=0x7f080004;
         public static final int TextView03=0x7f080001;
         public static final int btnAbort=0x7f08000d;
-        public static final int btnLogin=0x7f080011;
-        public static final int btnRead=0x7f080012;
+        public static final int btnBookRead=0x7f080014;
+        public static final int btnLogin=0x7f080013;
         public static final int btnRegister=0x7f08000c;
         public static final int edtAccount=0x7f080005;
         public static final int edtMemName=0x7f080002;
         public static final int edtPassword=0x7f080008;
         public static final int edtPassword2=0x7f08000b;
-        public static final int menu_settings=0x7f080013;
+        public static final int menu_settings=0x7f080015;
         public static final int tableRow1=0x7f080009;
-        public static final int tableRow2=0x7f08000f;
+        public static final int tableRow2=0x7f080011;
         public static final int textView1=0x7f08000a;
-        public static final int textView2=0x7f080010;
+        public static final int textView2=0x7f080012;
+        public static final int txtBookRead=0x7f08000f;
     }
     public static final class layout {
         public static final int add_mem=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int book_read=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int add_mem=0x7f070000;
+        public static final int book_read=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
@@ -57,7 +62,10 @@ public final class R {
         public static final int btn_register=0x7f050008;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int title_activity_add_mem=0x7f05000c;
+        public static final int title_activity_book_read=0x7f05000b;
         public static final int title_activity_main=0x7f050003;
+        public static final int title_activity_test=0x7f05000a;
         public static final int txt_account=0x7f050004;
         public static final int txt_mem_name=0x7f050006;
         public static final int txt_password=0x7f050005;
--- trunk/android/src/com/book/dandingbook/Main.java
+++ trunk/android/src/com/book/dandingbook/Main.java
@@ -1,17 +1,41 @@
 package com.book.dandingbook;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class Main extends Activity {
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ImageButton btnBookRead = (ImageButton)findViewById(R.id.btnBookRead);
+        ImageButton btnRegister = (ImageButton)findViewById(R.id.btnRegister);
+        
+        btnBookRead.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(Main.this, BookRead.class);
+				startActivity(intent);
+			}
+		});
+        
+        btnRegister.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(Main.this, AddMem.class);
+				startActivity(intent);
+			}
+		});
     }
 
     @Override
