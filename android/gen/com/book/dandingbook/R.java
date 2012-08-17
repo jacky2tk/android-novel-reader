8f8026f8edf58da685a8fe06fb9a144a1bb33382
jacky2tk@gmail.com <jacky2tk@gmail.com@56cb43c6-fa1d-8bbd-1b40-62c0ef1cc954>
2012-08-17 09:25:08 +0000

    1.新增背景音樂

--- android/gen/com/book/dandingbook/R.java
+++ android/gen/com/book/dandingbook/R.java
@@ -11,9 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int padding_large=0x7f040002;
-        public static final int padding_medium=0x7f040001;
-        public static final int padding_small=0x7f040000;
+        public static final int padding_large=0x7f050002;
+        public static final int padding_medium=0x7f050001;
+        public static final int padding_small=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
@@ -23,28 +23,28 @@ public final class R {
         public static final int menu3=0x7f020004;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f08000e;
-        public static final int TableLayout1=0x7f080010;
-        public static final int TableRow01=0x7f080006;
-        public static final int TableRow02=0x7f080003;
-        public static final int TableRow03=0x7f080000;
-        public static final int TextView01=0x7f080007;
-        public static final int TextView02=0x7f080004;
-        public static final int TextView03=0x7f080001;
-        public static final int btnAbort=0x7f08000d;
-        public static final int btnBookRead=0x7f080014;
-        public static final int btnLogin=0x7f080013;
-        public static final int btnRegister=0x7f08000c;
-        public static final int edtAccount=0x7f080005;
-        public static final int edtMemName=0x7f080002;
-        public static final int edtPassword=0x7f080008;
-        public static final int edtPassword2=0x7f08000b;
-        public static final int menu_settings=0x7f080015;
-        public static final int tableRow1=0x7f080009;
-        public static final int tableRow2=0x7f080011;
-        public static final int textView1=0x7f08000a;
-        public static final int textView2=0x7f080012;
-        public static final int txtBookRead=0x7f08000f;
+        public static final int LinearLayout1=0x7f09000e;
+        public static final int TableLayout1=0x7f090010;
+        public static final int TableRow01=0x7f090006;
+        public static final int TableRow02=0x7f090003;
+        public static final int TableRow03=0x7f090000;
+        public static final int TextView01=0x7f090007;
+        public static final int TextView02=0x7f090004;
+        public static final int TextView03=0x7f090001;
+        public static final int btnAbort=0x7f09000d;
+        public static final int btnBookRead=0x7f090014;
+        public static final int btnLogin=0x7f090013;
+        public static final int btnRegister=0x7f09000c;
+        public static final int edtAccount=0x7f090005;
+        public static final int edtMemName=0x7f090002;
+        public static final int edtPassword=0x7f090008;
+        public static final int edtPassword2=0x7f09000b;
+        public static final int menu_settings=0x7f090015;
+        public static final int tableRow1=0x7f090009;
+        public static final int tableRow2=0x7f090011;
+        public static final int textView1=0x7f09000a;
+        public static final int textView2=0x7f090012;
+        public static final int txtBookRead=0x7f09000f;
     }
     public static final class layout {
         public static final int add_mem=0x7f030000;
@@ -52,26 +52,29 @@ public final class R {
         public static final int main=0x7f030002;
     }
     public static final class menu {
-        public static final int add_mem=0x7f070000;
-        public static final int book_read=0x7f070001;
-        public static final int main=0x7f070002;
+        public static final int add_mem=0x7f080000;
+        public static final int book_read=0x7f080001;
+        public static final int main=0x7f080002;
+    }
+    public static final class raw {
+        public static final int ss=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int btn_abort=0x7f050009;
-        public static final int btn_register=0x7f050008;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int title_activity_add_mem=0x7f05000c;
-        public static final int title_activity_book_read=0x7f05000b;
-        public static final int title_activity_main=0x7f050003;
-        public static final int title_activity_test=0x7f05000a;
-        public static final int txt_account=0x7f050004;
-        public static final int txt_mem_name=0x7f050006;
-        public static final int txt_password=0x7f050005;
-        public static final int txt_password2=0x7f050007;
+        public static final int app_name=0x7f060000;
+        public static final int btn_abort=0x7f060009;
+        public static final int btn_register=0x7f060008;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int title_activity_add_mem=0x7f06000c;
+        public static final int title_activity_book_read=0x7f06000b;
+        public static final int title_activity_main=0x7f060003;
+        public static final int title_activity_test=0x7f06000a;
+        public static final int txt_account=0x7f060004;
+        public static final int txt_mem_name=0x7f060006;
+        public static final int txt_password=0x7f060005;
+        public static final int txt_password2=0x7f060007;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
--- android/src/com/book/dandingbook/BookRead.java
+++ android/src/com/book/dandingbook/BookRead.java
@@ -1,21 +1,59 @@
 package com.book.dandingbook;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.support.v4.app.NavUtils;
 
 public class BookRead extends Activity {
-
+	
+	MediaPlayer mPlayer; 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //²¾°£title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.book_read);
-    }
+        try{
+        	mPlayer = MediaPlayer.create(this, R.raw.ss); 
+        	mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC); 
+        	mPlayer.setLooping(true); 
+           }catch (IllegalStateException e) 
+	           {
+		       		e.printStackTrace(); 
+	           }
+}
+ 
+	@Override  protected void onResume()    
+    {  
+    	// TODO Auto-generated method stub            
+    	super.onResume();    
+    	mPlayer.start();   
+    } 
+    @Override  protected void onPause()   
+    {  
+    	// TODO Auto-generated method stub            
+    	super.onPause();   
+    	mPlayer.pause();   
+    } 
+    @Override  protected void onDestroy()   
+    {    
+    	// TODO Auto-generated method stub       
+    	super.onDestroy();       
+    	mPlayer.release();   
+    } 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
