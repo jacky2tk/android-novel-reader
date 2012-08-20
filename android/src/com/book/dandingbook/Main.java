2ff921d80156a7facf97c1260e91e0549ea60196
esther75952@gmail.com <esther75952@gmail.com@56cb43c6-fa1d-8bbd-1b40-62c0ef1cc954>
2012-08-20 03:38:14 +0000

    1.登入程式完成 2.註冊程式完成

--- android/src/com/book/dandingbook/Main.java
+++ android/src/com/book/dandingbook/Main.java
@@ -1,21 +1,29 @@
 package com.book.dandingbook;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends Activity {
 	private static final String TAG = "Danding_Main";
-	
+	private String strMain = "";
+	private String Message = "" ;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +50,16 @@ public class Main extends Activity {
 	private ImageButton btnLogin;
 	private ImageButton btnRegister;
 	private ImageButton btnBookRead;
+	private EditText txt_account ;
+	private EditText txt_pwd ;
 	
 	// 取得所有控制項
 	private void findViews() {
 		btnLogin = (ImageButton)findViewById(R.id.btnLogin);
         btnRegister = (ImageButton)findViewById(R.id.btnRegister);
         btnBookRead = (ImageButton)findViewById(R.id.btnBookRead);
+        txt_account = (EditText)findViewById(R.id.edtAccount) ;
+        txt_pwd = (EditText)findViewById(R.id.edtPassword) ;
 	}
 	
 	// 設定所有按鈕 Listener
@@ -56,8 +68,9 @@ public class Main extends Activity {
         btnLogin.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				Intent intent = new Intent(Main.this, BookList.class);
-				startActivity(intent);
+				
+				Message = "&account=" + txt_account.getText().toString() + "&pwd=" + txt_pwd.getText().toString() ; 
+				getMain(Message) ;
 			}
         	
         });
@@ -80,7 +93,54 @@ public class Main extends Activity {
 			}
 		});
 	}
-
+	final Handler DownloadHandler = new Handler();
+    final Runnable DownloadCallback = new Runnable() {
+		public void run() {
+			//Toast.makeText(AddMem.this, "Download Complete!", Toast.LENGTH_LONG).show();
+			Log.d(TAG, "Handler: " + strMain);
+			
+			TextView login_warn = (TextView)findViewById(R.id.login_warn) ;
+			login_warn.setText("") ;
+			if(strMain.equals("0")){  //判斷帳密是否輸入正確
+				login_warn.setText(R.string.login_warn) ;
+			}
+			else{
+				Intent intent = new Intent(Main.this, BookList.class);
+				startActivity(intent);
+			}
+		}
+    	
+    };
+	//檢查是否連上網路
+    private void getMain(final CharSequence contents) {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE); 
+    	if (Network.haveNetworkConnection(cm)){
+    		Thread thread = new Thread() {
+    			public void run() {
+    				try {
+    					
+    					if (Debug.On) Log.d(TAG, "Sent GET request");
+    					strMain = Network.getData(getString(R.string.agent_url), "case=user_login"+contents);
+    					
+    					if (Debug.On) Log.d(TAG, "Callback to main loop");
+    					DownloadHandler.post(DownloadCallback);
+    				} catch (Exception e) {
+    					//Log.e(TAG, e.getMessage());
+    					System.out.println(e.getMessage());
+    				}
+    			}
+    		};
+    		try {
+    			Log.d(TAG, "AddMem_Connectivity");
+	    	    thread.start();
+    		} catch (Exception e) {
+    			Log.e(TAG, e.getMessage());
+    			System.out.println(e.getMessage());
+    		}
+    	} else {
+    		Toast.makeText(Main.this, "請先連上網路!", Toast.LENGTH_LONG).show();
+    	}
+	}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
