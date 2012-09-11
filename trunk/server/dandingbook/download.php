05eff110831c760178854a071ab9db4ac4c851de
jacky2tk@gmail.com <jacky2tk@gmail.com@56cb43c6-fa1d-8bbd-1b40-62c0ef1cc954>
2012-09-11 13:51:57 +0000

    Server: 1. 刪除沒有對應的小說 9-13 2. 增加淡定書城的首頁 (含舊首頁 indexOld.html) 3. 增加 APK 檔案下載的 download.php 頁面

---
+++ trunk/server/dandingbook/download.php
@@ -0,0 +1,16 @@
+﻿<html>
+<head>
+<title>淡定書城</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<center>
+<img src="title-logo1.png" /><br>
+<img src="logo_s.png" /><br>
+<font size=20><a href="download/android/DandingBook.apk">下載 淡定書城 Android APK</a></font><br><br>
+</center>
+<font color="red" size=10>注意：安裝前必須要做的設定<br></font>
+<font size=10>設定 -> 應用程式 -> 勾選「未知的來源」<br></font>
+<iframe style="display:none">	<!-- 去除廣告 -->
+</body>
+</html>
